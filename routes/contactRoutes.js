0fbac8aa2814b18713c0be399b6b2baecc260c38
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2024-01-18 12:10:06 +0530

    Routing with Token Validation

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -6,7 +6,10 @@ const {
   updateContact,
   deleteContact,
 } = require("../controllers/contactController");
+const validateToken = require("../middleware/validateTokenHandler");
 const router = express.Router();
+
+router.use(validateToken);
 
 router.get("/", getContact);
 
